e7990e41907068716c041682a0c27b3de1e8dddd
apimook <apimook.yo.57@ubu.ac.th>
2017-09-07 09:39:54 +0700

    First commit

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<center><h2 style='color:red'>Hello World </h2> </n> ");
+    res.write("<h2 style='color:#80ff00'>apimook yoopoo </h2> </n>");
+    res.write("<h2 style='color:#0040ff'>5711403452</h2></center>");
     res.end('Hello World!');
 }).listen(8080);
